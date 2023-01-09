084fb6eb1382b77b07f1a29794ef17ab981cca62
michael seaman <mseaman26@gmail.com>
2023-01-08 22:41:08 -0800

    MVC folder structure

---
+++ utils/helpers.js
@@ -0,0 +1,22 @@
+module.exports = {
+    format_time: (date) => {
+      return date.toLocaleTimeString();
+    },
+    format_date: (date) => {
+      return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
+        new Date(date).getFullYear() + 5
+      }`;
+    },
+  };
+  
+  const helperObject = {
+    format_time: (date) => {
+      return date.toLocaleTimeString();
+    },
+    format_date: (date) => {
+      return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
+        new Date(date).getFullYear() + 5
+      }`;
+    },
+  };
+  
